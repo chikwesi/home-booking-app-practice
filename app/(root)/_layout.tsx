61232e1d8f31697c7aab461602380c67e5cefb44
admin <admin@admins-MacBook-Pro.local>
2025-07-26 16:46:51 +0100

    booking mobile app initial ui and login implementation

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,21 @@
+import {useGlobalContext} from "@/lib/global-provider";
+import {ActivityIndicator, SafeAreaView} from "react-native";
+import {Redirect, Slot} from "expo-router";
+
+export default function AppLayout() {
+    const {isLoggedIn, loading} = useGlobalContext();
+
+    if (loading) {
+        return (
+            <SafeAreaView className="bg-white h-full flex justify-center items-center">
+                <ActivityIndicator className='text-primary-300' size='large'/>
+            </SafeAreaView>
+        )
+    }
+
+    if (!isLoggedIn) {
+        return <Redirect href='/sign-in'/>
+    }
+
+    return <Slot/>
+}
